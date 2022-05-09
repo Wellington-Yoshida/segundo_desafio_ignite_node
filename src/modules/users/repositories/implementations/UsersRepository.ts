17f828049f6e32f3a2c78c425f2a2dcdb9326e95
Wellington-Yoshida <issami-yoshida@hotmail.com>
2022-05-09 19:20:14 -0300

    Realizado implementaçao da camanda Repository.

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,5 +1,6 @@
 import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+import { v4 as uuidV4 } from "uuid";
 
 class UsersRepository implements IUsersRepository {
   private users: User[];
@@ -19,23 +20,41 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    let newUser = new User();
+
+    newUser = {
+      id: uuidV4(),
+      name: name,
+      admin: "false",
+      email: email,
+      created_at: new Date(),
+      updated_at: new Date()
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user => user.email === email));
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.findById(receivedUser.id);
+    user.admin = "true";
+    user.updated_at = new Date();
+
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
